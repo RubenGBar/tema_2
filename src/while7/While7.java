ff30b857ae8ebdd432b642d9c3e53ef8329395ab
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-18 09:06:37 +0200

    18/10/2023

---
+++ src/while7/While7.java
@@ -0,0 +1,78 @@
+package while7;
+
+import java.util.Scanner;
+
+public class While7 {
+	public static void main(String[] args) {
+		
+		//Declaro las variables donde guardo el número introducido por el uuario y el número secreto
+		int num, random;
+		
+		//Creo el Scannerr
+		Scanner sc = new Scanner(System.in);
+		
+		//Genero el número secreto entre 1 y 100
+		random = (int) ((int) 1 + Math.random() * ((100 - 1) + 1));
+		
+		/*Le pido que introduzca un número entre 1 y 100 para adivinar el número secreto 
+		 * y que si se quiere rendir que introduzca -1*/
+		System.out.println("Introduzca un número entre el 1 y el 100, si se quiere "
+				+ "rendir introduzca -1: ");
+		
+		//Leo el valor de num del teclado
+		num = sc.nextInt();
+		
+		//Creo un condicional para que solo me pueda introducir valores entre 100 y -1
+		if (num < 100 && num >= -1) {
+			while (num != random) { /*Creo una variable que iterará mientras el número 
+									introducido sea distinto del secreto*/
+				
+				if (num == -1){/*Si introduce -1 como primer número aparece este mensaje 
+								con el número secreto*/
+					System.out.println("Vaya, te rindes? Había pensado en el número: " + random);
+					break;
+				} else if (num == 0){/*Si introduce 0 aparece este mensaje de error*/
+					System.out.println("El valor introducido es erróneo");
+					break;
+				} else if (num < random){/*Si introduce un número menor que el secreto aparece 
+										un mensaje que indica que el secreto es mayor y vuelvo a 
+										leer el siguinete número que introduzca junto con la 
+										opción para rendirse*/
+					System.out.println("El número es mayor");
+					System.out.println("Si se rinde introduzca -1");
+					num = sc.nextInt();
+					if (num == -1) {/*Si introduce -1 se enseña este mensaje con el mensaje secreto 
+					 				y el programa termina*/
+						System.out.println("Vaya, te rindes? Había pensado en el número: " + random);
+						break;
+					}
+				} else if (num > random){/*Si introduce un número mayor que el secreto aparece 
+										un mensaje que indica que el secreto es menor y vuelvo a 
+										leer el siguinete número que introduzca junto con la 
+										opción para rendirse*/
+					System.out.println("El número es menor");
+					System.out.println("Si se rinde introduzca -1");
+					if (num == -1) {/*Si introduce -1 se enseña este mensaje con el mensaje secreto 
+		 							y el programa termina*/
+						System.out.println("Vaya, te rindes? Había pensado en el número: " + random);
+						break;
+					}
+					//Vuelvo a leer el valor de num del teclado
+					num = sc.nextInt();
+				}
+			};
+		/*Si el número es igual al número secreto se imprime un mensaje con el 
+		 * que se indica que ha acertado*/
+		if (num == random) {
+			System.out.println("Acertaste!!");
+		}
+		}//Fin del if para que solo me introduzca valores entre 100 y -1 
+		else {//Si me introduce un valor que no esté entre 100 y -1 se imprime un mensaje de error
+			System.out.println("El valor introducido es erróneo");
+		}
+		
+		//Cierro el Scanner
+		sc.close();
+	}
+
+}
---
+++ src/while8/While8.java
@@ -0,0 +1,40 @@
+package while8;
+
+import java.util.Scanner;
+
+public class While8 {
+	public static void main(String[] args) {
+		
+		//Creo las variables donde guardo el árbol introducido y el árbol más alto
+		int arbolIntroducido = 0, arbolGuardar = 0;
+		
+		//Creo el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Creo un bucle que iterará mientras 
+		while (arbolIntroducido > -1) {
+			
+			/*Mensaje para que introduzca la altura de un árbol y las instrucciones para que 
+			 * sepa como terminar*/ 
+			System.out.println("Introduzca la altura de un árbol en centímetros y sin decimales, si "
+					+ "quiere terminar introduzca -1: ");
+			
+			/*Compruebo que el último valor introducido sea mayor que el guardado, si lo es guardo el 
+			 * nuevo valor introducido*/
+			if (arbolIntroducido > arbolGuardar) {
+				arbolGuardar = arbolIntroducido;
+			}
+			
+			//Leo el valor del arbol introducido del teclado
+			arbolIntroducido = sc.nextInt();
+			
+		}
+		
+		//Cuando termine el bucle muestro el valor del árbol más alto introducido
+		System.out.println("La altura del árbol más alto es de " + arbolGuardar + " metros");
+		
+		//Cierro el Scanner
+		sc.close();
+	}
+
+}
